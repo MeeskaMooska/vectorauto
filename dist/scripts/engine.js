294c2f4b9ca17eb3133aac1789ab4932d5f9b7cf
MeeskaMooska <77514959+MeeskaMooska@users.noreply.github.com>
2023-10-14 17:31:03 -0400

    Contact page is functional at desktop/laptop level, working on mobile.

--- dist/scripts/engine.js
+++ dist/scripts/engine.js
@@ -1,12 +1,14 @@
+// TODO fix menu button logic. It's not working properly. (not a big deal, but still)
 var menuActive = false;
 const menu = document.getElementById('header-link-container');
 
 function handleMenuButtonPress(menuButton) {
-    menuButton.classList.toggle('menu-button-change');
     if (menuActive) {
+        menuButton.classList.remove('menu-button-change');
         menu.style.display = 'none';
         menuActive = false;
     } else {
+        menuButton.classList.add('menu-button-change');
         menu.style.display = 'block';
         menuActive = true;
     }
@@ -14,10 +16,39 @@ function handleMenuButtonPress(menuButton) {
 };
 
 onresize = function() {
-    console.log("here");
     if (window.innerWidth > 700) {
-        document.getElementById("header-link-container").classList.toggle('menu-button-change');
         menuActive = false;
         menu.style.display = 'block';
+    } if (window.innerWidth < 700) {
+        document.getElementById("header-link-container").classList.remove('menu-button-change');
+        menu.style.display = 'none';
     }
 };
+
+function formSubmittedMessage() {
+    console.log('form submitted');
+}
+
+function inputFocusHandler(inputObject) {
+    let inputLabel = inputObject.previousElementSibling
+    inputLabel.classList.add('input-label-active');
+    inputLabel.style.color = 'red';
+}
+
+function inputBlurHandler(inputObject) {
+    let inputLabel = inputObject.previousElementSibling;
+    if (inputObject.value === '') {
+        inputLabel.classList.remove('input-label-active');
+    }
+    inputLabel.style.color = 'grey';
+}
+
+function textareaFocusHandler(inputObject) {
+    let inputLabel = inputObject.previousElementSibling
+    inputLabel.style.color = 'red';
+}
+
+function textareaBlurHandler(inputObject) {
+    let inputLabel = inputObject.previousElementSibling;
+    inputLabel.style.color = 'grey';
+}
